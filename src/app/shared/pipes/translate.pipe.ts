50ba58d2cadff62035707cb4c1694b0f9271ce5d
Fabian Bratzel <fbratzel@gmail.com>
2021-04-25 13:52:58 +0200

    first commit

---
+++ src/app/shared/pipes/translate.pipe.ts
@@ -0,0 +1,14 @@
+import { TaggedTemplateExpr } from "@angular/compiler";
+import { Pipe, PipeTransform } from "@angular/core";
+
+import lang from "../../../assets/i18n/de_DE.json";
+
+@Pipe({name: 'translate'})
+export class TranslatePipe implements PipeTransform {
+    transform(value: string, def?: string): string {
+        if ((<any>lang)[value] === undefined) {
+            return def === undefined ? "Not Found" : def;
+        }
+        return (<any>lang)[value];
+    }
+}
